86908f6b5712f3da4a98e524ceef8a7b386c8eaf
Fernando Perez <fernandohps17@gmail.com>
2023-11-04 11:43:21 -0400

    Se creo el componente card

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -7,12 +7,12 @@ export const Card = () => {
         <div className='card'>
             <figure className='card_figure'>
                 <div className='card_figure_add'>+</div>
-                <img src="https://cdn11.bigcommerce.com/s-k11cg5mzh9/products/437/images/137086/fc0078efc5dd4e2b579dd153ea420bc4865818c9cfeab31652d4e20abdd9a3ee__73308.1698681473.500.750.png?c=2" alt="" />
+                <img src="https://definicion.de/wp-content/uploads/2009/12/paisaje-1.jpg" alt="" />
                 <span className='card_figure_category'>Electronics</span>
             </figure>
             <p className='card_content'>
                 <span className='card_content_title'>Headphones</span>
-                <span className='card_content_price'>300$</span>
+                <span className='card_content_price'><b>300$</b></span>
             </p>
         </div>
     )
